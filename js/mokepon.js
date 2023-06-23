2b434bb5023d8f06bd9723badb15955880318c01
Douglas Ramirez <dorz.ram@gmail.com>
2023-06-22 20:23:08 -0400

    Mejorando el .js, incluyendo clases y objetos

--- js/mokepon.js
+++ js/mokepon.js
@@ -1,79 +1,112 @@
+const sectionAtaque = document.getElementById('seleccionar-ataque');
+const sectionReiniciar = document.getElementById('section-reiniciar');
+const btnMokepon = document.getElementById('btn-mokepon');
+const btnTierra = document.getElementById('btn-planta');
+const btnFuego = document.getElementById('btn-fuego');
+const btnAgua = document.getElementById('btn-agua');
+const btnReiniciarJuego = document.getElementById('btn-reiniciar');
+
+const seleccionarEquipo = document.getElementById('seleccionar-mokepon');
+const inputBarcelona = document.getElementById('barcelona'); // Esto es una manera de hacerlo
+const inputRealMadrid = document.getElementById('realMadrid');
+const inputArsenal = document.getElementById('realArsenal');
+const spanLogoJugador = document.getElementById('logoJugador');
+
+const spanLogoRival = document.getElementById('logoRival');
+
+const definicionAtaque = document.getElementById('resultadoDelPenal');
+
+const sectionMensajes = document.getElementById('resultadoDelPenal');
+
+const nuevasVidasJugador = document.getElementById('vidas-jugador');
+const nuevasVidasRival = document.getElementById('vidas-rival');
+
+const logoBarca = '<img src=./assets/pruebaBarcelona.png>'
+const logoMadrid = '<img src=./assets/pruebaMadrid.png>'
+const logoArsenal = '<img src=./assets/pruebaArsenal.png>'
+
 let ataqueJugador;
 let ataqueRival;
 let vidasJugador = 3;
 let vidasRival = 3;
+let spanNombreEquipo;
+let mokeponAleatorio;
+
+class Equipo {
+    constructor(nombre, logo, vida) {
+        this.nombre = nombre;
+        this.logo = logo;
+        this.vida = vida;
+    }
+}
+
+let barcelona = new Equipo('FCBarcelona', './assets/pruebaBarcelona.png', 3);
+let realMadrid = new Equipo('Real Madrid', './assets/pruebaMadrid', 3);
+let arsenal = new Equipo('Arsenal', './assets/pruebaArsenal.png', 3);
+
 
 function iniciarJuego() {
 
-    let sectionAtaque = document.getElementById('seleccionar-ataque');
-    sectionAtaque.style.display = 'none';
-
-    let sectionReiniciar = document.getElementById('section-reiniciar');
+    sectionAtaque.style.display = 'none'; // Esto es para que no se vea la seccion de ataque al iniciar el juego
     sectionReiniciar.style.display = 'none';
 
-    let btnMokepon = document.getElementById('btn-mokepon');
     btnMokepon.addEventListener('click', seleccionarMokepon);
-
-    let btnTierra = document.getElementById('btn-planta');
     btnTierra.addEventListener('click', ataquePlanta);
-
-    let btnFuego = document.getElementById('btn-fuego');
     btnFuego.addEventListener('click', ataqueFuego);
-
-    let btnAgua = document.getElementById('btn-agua');
     btnAgua.addEventListener('click', ataqueAgua);
-
-    let btnReiniciarJuego = document.getElementById('btn-reiniciar');
     btnReiniciarJuego.addEventListener('click', reiniciarJuego);
-
 }
 
 function seleccionarMokepon() {
-    let seleccionarMokepon = document.getElementById('seleccionar-mokepon');
-    seleccionarMokepon.style.display = 'none';
+    seleccionarEquipo.style.display = 'none';
+    sectionAtaque.style.display = 'flex';
+    // let spanNombreEquipo = document.getElementById('nombre-mokepon-jugador');
 
-    let sectionAtaque = document.getElementById('seleccionar-ataque');
-    sectionAtaque.style.display = 'block';
-
-    let inputhipodoge = document.getElementById('hipodoge'); // Esto es una manera de hacerlo
-    let spanNombreMokeponJugador = document.getElementById('nombre-mokepon-jugador');
-
-    if (inputhipodoge.checked) {
-        spanNombreMokeponJugador.innerHTML = 'Hipodoge';
-    } else if (capipepo.checked) { // o esta es la otra manera, buscando por el id directamente
-        spanNombreMokeponJugador.innerHTML = 'Capipepo';
-    } else if (ratigueya.checked) {
-        spanNombreMokeponJugador.innerHTML = 'Ratigueya';
-    } else if (langostelvis.checked) {
-        spanNombreMokeponJugador.innerHTML = 'Langostelvis';
-    } else if (tucapalma.checked) {
-        spanNombreMokeponJugador.innerHTML = 'Tucapalma';
-    } else if (pydos.checked) {
-        spanNombreMokeponJugador.innerHTML = 'Pydos';
-    } else {
-        alert("DEBES SELECCIONAR UN MOKEPON")
+    if (inputBarcelona.checked) {
+        //spanNombreEquipo.innerHTML = 'FCBarcelona';
+        //spanLogoEquipo.innerHTML = '<img src=./assets/pruebaBarcelona.png>';
+        spanLogoJugador.innerHTML = logoBarca;
+    } else if (inputRealMadrid.checked) { // o esta es la otra manera, buscando por el id directamente
+        //spanNombreEquipo.innerHTML = 'Real Madrid';
+        spanLogoJugador.innerHTML = logoMadrid;
+        //} else if (ratigueya.checked) {
+        //  spanNombreMokeponJugador.innerHTML = 'Ratigueya';
+        //} else if (langostelvis.checked) {
+        //  spanNombreMokeponJugador.innerHTML = 'Langostelvis';
+        //} else if (tucapalma.checked) {
+        //  spanNombreMokeponJugador.innerHTML = 'Tucapalma';
+    } else if (inputArsenal.checked) {
+        //spanNombreEquipo.innerHTML = 'Arsenal';
+        spanLogoJugador.innerHTML = logoArsenal;
+    } else if (inputBarcelona.checked == false
+        && inputRealMadrid.checked == false
+        && inputArsenal.checked == false) {
+        alert("DEBES SELECCIONAR UN EQUIPO");
+        location.reload();
     }
-
     mokeponRival()
 }
 
 function mokeponRival() {
 
-    let mokeponAleatorio = aleatorio(1, 6);
-    let spanNombreMokeponRival = document.getElementById('nombre-mokepon-rival');
+    let mokeponAleatorio = aleatorio(1, 3);
+    // let spanNombreMokeponRival = document.getElementById('nombre-mokepon-rival');
 
     if (mokeponAleatorio == 1) {
-        spanNombreMokeponRival.innerHTML = 'Hipodoge';
+        //spanNombreMokeponRival.innerHTML = 'FCBarcelona';
+        spanLogoRival.innerHTML = logoBarca;
     } else if (mokeponAleatorio == 2) {
-        spanNombreMokeponRival.innerHTML = 'Capipepo';
-    } else if (mokeponAleatorio == 3) {
+        //spanNombreMokeponRival.innerHTML = 'Real Madrid';
+        spanLogoRival.innerHTML = logoMadrid;
+    } /*else if (mokeponAleatorio == 3) {
         spanNombreMokeponRival.innerHTML = 'Ratigueya';
     } else if (mokeponAleatorio == 4) {
         spanNombreMokeponRival.innerHTML = 'Langostelvis';
     } else if (mokeponAleatorio == 5) {
         spanNombreMokeponRival.innerHTML = 'Tucapalma';
-    } else if (mokeponAleatorio == 6) {
-        spanNombreMokeponRival.innerHTML = 'Pydos'
+    }*/ else if (mokeponAleatorio == 3) {
+        //spanNombreMokeponRival.innerHTML = 'Arsenal'
+        spanLogoRival.innerHTML = logoArsenal;
     }
 }
 
@@ -81,18 +114,18 @@ function aleatorio(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-function ataquePlanta() {
-    ataqueJugador = '🌿';
+function ataquePlanta() { // Control z hasta aqui
+    ataqueJugador = 'izquierda';
     ataqueAleatorioRival();
 }
 
 function ataqueFuego() {
-    ataqueJugador = '🔥';
+    ataqueJugador = 'centro';
     ataqueAleatorioRival();
 }
 
 function ataqueAgua() {
-    ataqueJugador = '💧'
+    ataqueJugador = 'derecha'
     ataqueAleatorioRival();
 }
 
@@ -100,100 +133,104 @@ function ataqueAleatorioRival() {
     let ataqueAleatorio = aleatorio(1, 3);
 
     if (ataqueAleatorio == 1) {
-        ataqueRival = '🌿';
+        ataqueRival = 'izquierda';
     } else if (ataqueAleatorio == 2) {
-        ataqueRival = '🔥';
+        ataqueRival = 'centro';
     } else if (ataqueAleatorio == 3) {
-        ataqueRival = '💧';
+        ataqueRival = 'derecha';
     }
     resultadoDelAtaque();
 }
 
 function resultadoDelAtaque() {
     if (ataqueJugador == ataqueRival) {
-        crearMensaje("EMPATE: 🙍🏻‍♂️ 🤖");
-    } else if (ataqueJugador == '🌿' && ataqueRival == '💧') {
-        crearMensaje("GANADOR: 🙍🏻‍♂️ => 🌿");
-    } else if (ataqueJugador == '🔥' && ataqueRival == '🌿') {
-        crearMensaje("GANADOR: 🙍🏻‍♂️ => 🔥");
-    } else if (ataqueJugador == '💧' && ataqueRival == '🔥') {
-        crearMensaje("GANADOR: 🙍🏻‍♂️ => 💧");
+        crearMensaje("PENAL TAPADO 🧤");
+        console.log("0" + ataqueJugador + "vs" + ataqueRival);
     } else {
-        crearMensaje("GANADOR: 🤖 => " + ataqueRival);
+        console.log("4" + ataqueJugador + "vs" + ataqueRival);
+        crearMensaje("GOOOOOOL ⚽️ ");
     }
     contadorVidas();
 }
 
 function crearMensaje(resultado) {
-    let sectionMensajes = document.getElementById('section-mensaje');
+    //let sectionMensajes = document.getElementById('section-mensaje');
 
-    let mensaje = document.createElement('p');
-    mensaje.innerHTML = ('🙍🏻‍♂️ : ' + ataqueJugador + ' vs ' + ataqueRival + ' : 🤖');
+    /*let mensaje = document.createElement('p');
+    mensaje.innerHTML = ('🙍🏻‍♂️ : ' + ataqueJugador + ' vs ' + ataqueRival + ' : 🤖');*/
 
-    let definicionAtaque = document.createElement('p');
+    //let definicionAtaque = document.createElement('p');
     definicionAtaque.innerHTML = (resultado);
 
-    sectionMensajes.appendChild(mensaje);
-    sectionMensajes.appendChild(definicionAtaque);
+    //sectionMensajes.appendChild(mensaje);
+    //sectionMensajes.appendChild(definicionAtaque);
 }
 
 function crearMensajeFinal(resultadoFinal) {
-    let sectionMensajes = document.getElementById('section-mensaje');
+    /*let sectionMensajes = document.getElementById('section-mensaje');
     let mensaje = document.createElement('p');
     mensaje.innerHTML = resultadoFinal;
-    sectionMensajes.appendChild(mensaje);
+    sectionMensajes.appendChild(mensaje); */
 
-    let sectionReiniciar = document.getElementById('section-reiniciar');
+    sectionMensajes.innerHTML = resultadoFinal;
     sectionReiniciar.style.display = 'block';
 }
 
 function contadorVidas() {
-    let nuevasVidasJugador = document.getElementById('vidas-jugador');
-    let nuevasVidasRival = document.getElementById('vidas-rival');
-
     if (ataqueJugador == ataqueRival) {
-
-    } else if ((ataqueJugador == '🌿' && ataqueRival == '💧')
-        || (ataqueJugador == '🔥' && ataqueRival == '🌿')
-        || (ataqueJugador == '💧' && ataqueRival == '🔥')) {
-
-        vidasRival--;
-        if (vidasRival == 2) {
-            nuevasVidasRival.innerHTML = ('❤️❤️');
-        } else if (vidasRival == 1) {
-            nuevasVidasRival.innerHTML = ('❤️');
-        } else if (vidasRival == 0) {
-            nuevasVidasRival.innerHTML = ('');
-            crearMensajeFinal('HAS GANADO LA BATALLA');
-            desactivarBotones();
-        }
-
-    } else {
         vidasJugador--;
         if (vidasJugador == 2) {
-            nuevasVidasJugador.innerHTML = ('❤️❤️');
+            nuevasVidasJugador.innerHTML = ('⚽️⚽️❌');
         } else if (vidasJugador == 1) {
-            nuevasVidasJugador.innerHTML = ('❤️');
+            nuevasVidasJugador.innerHTML = ('⚽️❌❌');
         } else if (vidasJugador == 0) {
-            nuevasVidasJugador.innerHTML = ('');
-            crearMensajeFinal('TU MOKEPON HA MUERTO');
+            nuevasVidasJugador.innerHTML = ('❌❌❌');
+            crearMensajeFinal('PERDISTE');
             desactivarBotones();
         }
+    } else {
+        vidasRival--;
+        if (vidasRival == 2) {
+            nuevasVidasRival.innerHTML = ('❌⚽️⚽️');
+        } else if (vidasRival == 1) {
+            nuevasVidasRival.innerHTML = ('❌❌⚽️');
+        } else if (vidasRival == 0) {
+            nuevasVidasRival.innerHTML = ('❌❌❌');
+            crearMensajeFinal('CAMPEON 🏆');
+            desactivarBotones();
+        }
+    } /*else if ((ataqueJugador == '🌿' && ataqueRival == '💧')
+        || (ataqueJugador == '🔥' && ataqueRival == '🌿')
+        || (ataqueJugador == '💧' && ataqueRival == '🔥')) {*/
+
+    /* vidasRival--;
+    if (vidasRival == 2) {
+        nuevasVidasRival.innerHTML = ('❤️❤️');
+    } else if (vidasRival == 1) {
+        nuevasVidasRival.innerHTML = ('❤️');
+    } else if (vidasRival == 0) {
+        nuevasVidasRival.innerHTML = ('');
+        crearMensajeFinal('HAS GANADO LA BATALLA');
+        desactivarBotones();
     }
 
+} else {
+    vidasJugador--;
+    if (vidasJugador == 2) {
+        nuevasVidasJugador.innerHTML = ('❤️❤️');
+    } else if (vidasJugador == 1) {
+        nuevasVidasJugador.innerHTML = ('❤️');
+    } else if (vidasJugador == 0) {
+        nuevasVidasJugador.innerHTML = ('');
+        crearMensajeFinal('TU MOKEPON HA MUERTO');
+        desactivarBotones();
+    }*/
 }
 
 function desactivarBotones() {
-
-    let btnTierra = document.getElementById('btn-planta');
     btnTierra.disabled = true;
-
-    let btnFuego = document.getElementById('btn-fuego');
     btnFuego.disabled = true;
-
-    let btnAgua = document.getElementById('btn-agua');
     btnAgua.disabled = true;
-
 }
 function reiniciarJuego() {
     location.reload();
